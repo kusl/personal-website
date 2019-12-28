c5d0183c6ef4e34a9f45b39ebc32437d128813df
Kushal <kushaldeveloper@gmail.com>
2019-12-27 17:37:42 -0700

    use item component for the demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { LookupStateService } from './lookup-state-service.service';
-import { HTTP_STATUS_CODE } from './http-status-code.enum';
 import { ProjectType } from './project-type.model';
-import { ProjectTypeService } from './project-type.service';
 import { LookupState } from './lookup-state.model';
 import { Subproject } from './subproject.model';
-import { SubprojectService } from './subproject.service';
 
 @Component({
   selector: 'app-root',
@@ -18,57 +14,7 @@ export class AppComponent {
   lookupStates: Array<LookupState>;
   subprojects: Array<Subproject>;
   lookupProjectTypes: Array<ProjectType>;
-  public constructor(private titleService: Title,
-    private lookupStateService: LookupStateService,
-    private projectTypeService: ProjectTypeService,
-    private subprojectService: SubprojectService) {
+  public constructor(private titleService: Title) {
     this.titleService.setTitle(this.title);
-    this.getLookupStates();
-    this.getLookupProjectTypes();
-    this.getSubprojects();
-  }
-  getLookupStates() {
-    this.projectTypeService.getList().subscribe(response => {
-      if (response.status === HTTP_STATUS_CODE.OK && response.data) {
-        if (!this.lookupProjectTypes) {
-          this.lookupProjectTypes = new Array<ProjectType>();
-        }
-        response.data.forEach(x => {
-          this.lookupProjectTypes.push(x.data);
-        });
-        console.log({ types: this.lookupProjectTypes });
-      } else {
-        console.log(response);
-      }
-    });
-  }
-  getLookupProjectTypes() {
-    this.subprojectService.getList().subscribe(response => {
-      if (response.status === HTTP_STATUS_CODE.OK && response.data) {
-        if (!this.subprojects) {
-          this.subprojects = new Array<Subproject>();
-        }
-        response.data.forEach(x => {
-          this.subprojects.push(x.data);
-        });
-        console.log({ subprojects: this.subprojects });
-      } else {
-        console.log(response);
-      }
-    });
-  }
-  getSubprojects() {
-    this.subprojectService.getList().subscribe(response => {
-      if (response.status === HTTP_STATUS_CODE.OK && response.data) {
-        if (!this.subprojects) {
-          this.subprojects = new Array<Subproject>();
-        }
-        response.data.forEach(x => {
-          this.subprojects.push(x.data);
-        });
-      } else {
-        console.log(response);
-      }
-    })
   }
 }
--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { LookupState } from '../lookup-state.model';
+import { Subproject } from '../subproject.model';
+import { ProjectType } from '../project-type.model';
+import { Title } from '@angular/platform-browser';
+import { LookupStateService } from '../lookup-state-service.service';
+import { ProjectTypeService } from '../project-type.service';
+import { SubprojectService } from '../subproject.service';
+import { HTTP_STATUS_CODE } from '../http-status-code.enum';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +14,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.sass']
 })
 export class ItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.titleService.setTitle(this.title);
+    this.getLookupStates();
+    this.getLookupProjectTypes();
+    this.getSubprojects();
   }
-
+  title = 'Personal Website';
+  lookupStates: Array<LookupState>;
+  subprojects: Array<Subproject>;
+  lookupProjectTypes: Array<ProjectType>;
+  public constructor(private titleService: Title,
+    private lookupStateService: LookupStateService,
+    private projectTypeService: ProjectTypeService,
+    private subprojectService: SubprojectService) {
+  }
+  getLookupStates() {
+    this.lookupStateService.getList().subscribe(response => {
+      if (response.status === HTTP_STATUS_CODE.OK && response.data) {
+        if (!this.lookupStates) {
+          this.lookupStates = new Array<LookupState>();
+        }
+        response.data.forEach(x => {
+          this.lookupStates.push(x.data);
+        });
+        console.log({ types: this.lookupProjectTypes });
+      } else {
+        console.log(response);
+      }
+    });
+  }
+  getLookupProjectTypes() {
+    this.projectTypeService.getList().subscribe(response => {
+      if (response.status === HTTP_STATUS_CODE.OK && response.data) {
+        if (!this.lookupProjectTypes) {
+          this.lookupProjectTypes = new Array<ProjectType>();
+        }
+        response.data.forEach(x => {
+          this.lookupProjectTypes.push(x.data);
+        });
+        console.log({ projectTypes: this.lookupProjectTypes });
+      } else {
+        console.log(response);
+      }
+    });
+  }
+  getSubprojects() {
+    this.subprojectService.getList().subscribe(response => {
+      if (response.status === HTTP_STATUS_CODE.OK && response.data) {
+        if (!this.subprojects) {
+          this.subprojects = new Array<Subproject>();
+        }
+        response.data.forEach(x => {
+          this.subprojects.push(x.data);
+        });
+      } else {
+        console.log(response);
+      }
+    })
+  }
 }
